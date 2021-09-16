7f83052564459b1534c3ea84bfe6b009d8926c25
shinji <inu.koro56@gmail.com>
2021-09-17 02:46:34 +0900

    Firebaseにアプリのボタンからデータを追加

---
+++ lib/add_book/add_book_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'add_book_model.dart';
+
+class AddBookPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<AddBookModel>(
+      create: (_) => AddBookModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('本一覧'),
+        ),
+        body: Center(
+            child: Consumer<AddBookModel>(builder: (context, model, child) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                    hintText: '本のタイトル'
+                    ),
+                    onChanged: (text) {
+                      model.title = text;
+                    },
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                        hintText: '本の著者'
+                    ),
+                    onChanged: (text) {
+                      model.author = text;
+                    },
+                  ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                  ElevatedButton(onPressed: () async {
+                    //追加の処理
+                    try {
+                      await model.addBook();
+                      Navigator.of(context).pop(true);
+                    } catch(e) {
+                      final snackBar = SnackBar(
+                        backgroundColor: Colors.red,
+                        content: Text(e.toString()),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                    }, child: Text('追加する'),
+                  ),
+                ],
+                ),
+              );
+            })
+        ),
+      ),
+    );
+  }
+}
--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/add_book/add_book_page.dart';
 import 'package:book_app/domain/book.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -34,10 +35,29 @@ class BookListPage extends StatelessWidget {
             );
           })
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
+        floatingActionButton: Consumer<BookListModel>(builder: (context, model, child) {
+            return FloatingActionButton(
+              onPressed: () async {
+                final bool? added = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddBookPage(),
+                    fullscreenDialog: true,
+                  ),
+                );
+                if (added != null && added) {
+                  final snackBar = SnackBar(
+                    backgroundColor: Colors.green,
+                    content: Text('本を追加しました'),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+                model.fetchBookList();
+              },
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            );
+          }
         ),
       ),
     );
